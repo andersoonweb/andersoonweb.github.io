5f9d613e00d589b6525fc725ac273b2227e9ecaf
Anderson Menezes <andersomxp@gmail.com>
2015-05-13 20:27:54 -0300

    Remoção de código não usados e correções de alinhamentos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,4 @@
 
-// Script Wow
-	new WOW().init();
-// Function facebook share
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.0";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-
-// Function twitter share
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-
-// Function Google pluss share
-window.___gcfg = {lang: 'pt-BR'};
-
-  (function() {
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/platform.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-  })();
 
 
 
